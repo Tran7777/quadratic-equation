c1c210f624e2bb2d112f08cec36a051aa6b0fdaa
Tran7777 <30432795+Tran7777@users.noreply.github.com>
2018-02-27 23:40:03 +0300

    Update index.js
    should work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const ratioRegexp = /(\+?|\-?)\d+/g;
+    const parsedEquation = equation.split(' ').join('').replace('*x^2', '').replace('*x', '');
+
+    const a = +ratioRegexp.exec(parsedEquation)[0];
+    const b = +ratioRegexp.exec(parsedEquation)[0];
+    const c = +ratioRegexp.exec(parsedEquation)[0];
+
+    const d = Math.pow(b, 2) - 4 * a * c;
+
+    const xOne = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    const xTwo = Math.round((-b - Math.sqrt(d)) / (2 * a));
+
+    return [xOne, xTwo].sort((left, right) => left - right);
+
 }
